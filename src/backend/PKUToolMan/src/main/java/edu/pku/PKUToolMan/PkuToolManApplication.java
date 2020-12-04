1c298c918fe82d97278085476238808850ad9b4b
Sihan-Wang <824908786@qq.com>
2020-12-04 19:35:01 +0800

    add backend structure

---
+++ src/backend/PKUToolMan/src/main/java/edu/pku/PKUToolMan/PkuToolManApplication.java
@@ -0,0 +1,13 @@
+package edu.pku.PKUToolMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkuToolManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkuToolManApplication.class, args);
+	}
+
+}
