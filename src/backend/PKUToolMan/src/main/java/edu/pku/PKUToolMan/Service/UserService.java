3ec968a0b1265a6e372b9858260cb61e3884b16b
liuyunhui123 <liuyunhui2009910@hotmail.com>
2020-12-13 13:03:58 +0800

    cleanup for directory and imports

--- src/backend/PKUToolMan/src/main/java/edu/pku/PKUToolMan/Service/UserService.java
+++ src/backend/PKUToolMan/src/main/java/edu/pku/PKUToolMan/Service/UserService.java
@@ -1,8 +1,6 @@
 package edu.pku.PKUToolMan.Service;
 
 import edu.pku.PKUToolMan.Entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 public interface UserService {
     public void create(User user);
