70c38a27a2c379b03d3079e9e8d1333cc0aeabef
liuyunhui123 <liuyunhui2009910@hotmail.com>
2020-12-16 16:36:30 +0800

    unit tests.

--- src/backend/PKUToolMan/src/test/java/edu/pku/PKUToolMan/PkuToolManApplicationTests.java
+++ src/backend/PKUToolMan/src/test/java/edu/pku/PKUToolMan/PkuToolManApplicationTests.java
@@ -26,10 +26,62 @@ class UserDAOTest {
 	private UserDAO userDAO;
 
 	@Test
+	public void createTest() {
+		User user = new User("liuliuliu", "123123",
+				"yunhui@pku.edu.cn", "18501850185");
+		try {
+			userDAO.create(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			user = userDAO.queryByNickname("liuliuliu");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
 	public void queryByIdTest() {
-		User user = null;
+		User user;
+
+		// exists
+		user = null;
 		try {
 			user = userDAO.queryById(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userDAO.queryById(10);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void queryByNicknameTest() {
+		User user;
+
+		// exists
+		user = null;
+		try {
+			user = userDAO.queryByNickname("liuyunhui");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userDAO.queryByNickname("liutwohui");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,13 +90,86 @@ class UserDAOTest {
 
 	@Test
 	public void queryByEmailTest() {
-		User user = null;
+		User user;
+
+		// exists
+		user = null;
 		try {
 			user = userDAO.queryByEmail("liu@pku.edu.cn");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userDAO.queryByEmail("liuliu@pku.edu.cn");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void queryByPhoneNumTest() {
+		User user;
+
+		// exists
+		user = null;
+		try {
+			user = userDAO.queryByPhoneNum("18501232877");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userDAO.queryByPhoneNum("18512345678");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void modifyWithTest() {
+		User modifyUser = new User(
+				4, "liuliuliu",
+				"123123",
+				"yunhui@pku.edu.cn", "18501850185"
+		);
+		User modifiedUser = null;
+
+		// modify into
+		try {
+			modifyUser.setNickname("liuliu");
+			userDAO.modifyWith(modifyUser);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			modifiedUser = userDAO.queryById(modifyUser.getid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(modifiedUser);
+
+		// modify back
+		try {
+			modifyUser.setNickname("liuliuliu");
+			userDAO.modifyWith(modifyUser);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			modifiedUser = userDAO.queryById(modifyUser.getid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(modifiedUser);
 	}
 }
 
@@ -54,10 +179,64 @@ class UserServiceImplTest {
 	private UserServiceImpl userService;
 
 	@Test
+	public void createTest() {
+		User user = new User(
+				"yunyunyun", "123123",
+				"liuliu@pku.edu.cn", "18522342234"
+		);
+		try {
+			userService.create(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			user = userService.queryByNickname("yunyunyun");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
 	public void queryByIdTest() {
-		User user = null;
+		User user;
+
+		// exists
+		user = null;
 		try {
 			user = userService.queryById(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userService.queryById(10);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void queryByNicknameTest() {
+		User user;
+
+		// exists
+		user = null;
+		try {
+			user = userService.queryByNickname("liuyunhui");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userService.queryByNickname("yunhuiliu");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -66,12 +245,84 @@ class UserServiceImplTest {
 
 	@Test
 	public void queryByEmailTest() {
-		User user = null;
+		User user;
+
+		// exists
+		user = null;
 		try {
 			user = userService.queryByEmail("liu@pku.edu.cn");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userService.queryByEmail("huihui@pku.edu.cn");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void queryByPhoneNumTest() {
+		User user;
+
+		// exists
+		user = null;
+		try {
+			user = userService.queryByPhoneNum("18501232877");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+
+		// not exists
+		user = null;
+		try {
+			user = userService.queryByEmail("18500000000");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+	}
+
+	@Test
+	public void modifyWithTest() {
+		User modifyUser = new User(
+				5, "yunyunyun", "123123",
+				"liuliu@pku.edu.cn", "18522342234"
+		);
+		User modifiedUser = null;
+
+		// modify into
+		try {
+			modifyUser.setNickname("liuliuliu");
+			userService.modifyWith(modifyUser);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			modifiedUser = userService.queryById(modifyUser.getid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(modifiedUser);
+
+		// modify back
+		try {
+			modifyUser.setNickname("yunyunyun");
+			userService.modifyWith(modifyUser);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			modifiedUser = userService.queryById(modifyUser.getid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(modifiedUser);
 	}
 }
