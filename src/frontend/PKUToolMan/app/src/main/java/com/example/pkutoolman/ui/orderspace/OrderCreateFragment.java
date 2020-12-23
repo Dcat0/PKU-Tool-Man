cc07cd4081c8b1a3380f1dbb72cec41efa4237d5
Dcat0 <460755725@qq.com>
2020-12-23 13:35:04 +0800

    上一个commit没有提交完整…再次补充提交

--- src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateFragment.java
+++ src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateFragment.java
@@ -25,7 +25,7 @@ public class OrderCreateFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.order_create_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_order_create, container, false);
     }
 
     @Override
