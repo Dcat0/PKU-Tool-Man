9c3a3cc622a3510e1358e6913bdcafe6b39e45c1
Dcat0 <460755725@qq.com>
2020-12-24 03:38:33 +0800

    完成了”创建订单“的界面，还需进一步加入事件处理

--- src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateFragment.java
+++ src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateFragment.java
@@ -7,17 +7,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.pkutoolman.R;
 
 public class OrderCreateFragment extends Fragment {
 
     private OrderCreateViewModel mViewModel;
-
+    private RadioGroup rg;
+    private ImageView btnBack;
     public static OrderCreateFragment newInstance() {
         return new OrderCreateFragment();
     }
@@ -25,7 +32,30 @@ public class OrderCreateFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_order_create, container, false);
+        //return inflater.inflate(R.layout.fragment_order_create, container, false);
+        View root = inflater.inflate(R.layout.fragment_order_create, container, false);
+        //获得组件
+        btnBack = root.findViewById(R.id.btn_back_ordercreate);
+        rg = root.findViewById(R.id.orderType_rg);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkid) {
+                RadioButton rb_temp = root.findViewById(radioGroup.getCheckedRadioButtonId());
+                switch (checkid){
+                    case R.id.orderType_rbt1:
+                        Toast.makeText(getContext(),"取快递", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.orderType_rbt2:
+                        Toast.makeText(getContext(), rb_temp.getText(),Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.orderType_rbt3:
+                        Toast.makeText(getContext(), rb_temp.getText(),Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+        setButtonListener();
+        return root;
     }
 
     @Override
@@ -35,4 +65,13 @@ public class OrderCreateFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    private void setButtonListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //返回上一界面
+                Navigation.findNavController(v).navigateUp();
+            }
+        });
+    }
 }
