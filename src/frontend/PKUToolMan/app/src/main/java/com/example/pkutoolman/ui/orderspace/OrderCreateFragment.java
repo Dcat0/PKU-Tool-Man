e8990052ea39a6f723599dfce809562cfd570fc0
Dcat0 <460755725@qq.com>
2020-12-23 12:34:43 +0800

    新建了“订单创建“页面，并增加了跳转。（该页面内容还需填充）

---
+++ src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateFragment.java
@@ -0,0 +1,38 @@
+package com.example.pkutoolman.ui.orderspace;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pkutoolman.R;
+
+public class OrderCreateFragment extends Fragment {
+
+    private OrderCreateViewModel mViewModel;
+
+    public static OrderCreateFragment newInstance() {
+        return new OrderCreateFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.order_create_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(OrderCreateViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
