8a60f296a0de796d2229d98747c8aaa4e649d738
andcgt <73420481+andcgt@users.noreply.github.com>
2020-12-24 16:42:16 +0800

    add chat again

--- src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/orderspace/OrderCreateViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.pkutoolman.ui.orderspace;
-
-import androidx.lifecycle.ViewModel;
-
-public class OrderCreateViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
