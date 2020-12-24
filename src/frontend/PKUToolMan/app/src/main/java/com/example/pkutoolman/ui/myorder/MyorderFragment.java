8299a361429ef3c6f8752ad483fe6dfb16f31c07
Pakchoi-i <1800012967@pku.edu.cn>
2020-12-24 16:59:35 +0800

    与主分支同步

--- src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/myorder/MyorderFragment.java
+++ src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/ui/myorder/MyorderFragment.java
@@ -1,9 +1,7 @@
 package com.example.pkutoolman.ui.myorder;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,40 +9,18 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.example.pkutoolman.MainActivity;
 import com.example.pkutoolman.R;
 import com.example.pkutoolman.baseclass.Order;
-import com.example.pkutoolman.baseclass.Data;
-import com.example.pkutoolman.ui.PublishActivity;
 import com.example.pkutoolman.ui.orderinfo.OrderinfoActivity;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.StandardSocketOptions;
-import java.net.URL;
-import java.net.URLConnection;
-import java.security.interfaces.RSAMultiPrimePrivateCrtKey;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MyorderFragment extends Fragment {
