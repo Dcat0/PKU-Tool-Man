9ea0464a5214a8a88521f4fee47c526e0ca92a3a
gosicksky <1700012724@pku.edu.cn>
2020-12-24 16:01:45 +0800

    完成与主分支合并

--- src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/baseclass/Data.java
+++ src/frontend/PKUToolMan/app/src/main/java/com/example/pkutoolman/baseclass/Data.java
@@ -8,10 +8,7 @@ import java.util.List;
 public class Data {
     private static int userID;
     private static String nickName;
-<<<<<<< HEAD
-=======
     private static String token;
->>>>>>> zkm--interface
     private static List<Order> myOrderInfo = new ArrayList<Order>();
 
     public static void setUserID(int _userID) {
@@ -31,9 +28,6 @@ public class Data {
     public static String getToken(){
         return token;
     }
-
-    public static void setNickName(String name) { nickName = name; }
-    public static String getNickName() { return nickName; }
 
     public static void setMyOrderInfo(List<Order> a) {
         myOrderInfo = a;
